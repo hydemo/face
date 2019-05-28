c0d5e0539dfea45acdcb4845dc41547fe67c5d14
欧阳旭靖 <>
2019-05-28 17:07:24 +0800

    初始化脚本

--- src/init/init.service.ts
+++ src/init/init.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as moment from 'moment';
+import * as md5 from 'md5';
 import { CreateAdminDTO } from 'src/module/admin/dto/admin.dto';
 import { AdminService } from 'src/module/admin/admin.service';
 import { SOCUtil } from 'src/utils/soc.util';
@@ -12,20 +13,16 @@ export class InitService {
   ) { }
 
   async init() {
-    // const data = await this.socUtil.qrcodeAddress('1A814683-14F8-6129-E054-90E2BA548A34')
-    // console.log(data, 'data')
-    // const adminExist = await this.adminService.countByCondition({ role: 0 })
-    // if (!adminExist) {
-    //   const admin: CreateAdminDTO = {
-    //     nickname: '超级管理员',
-    //     role: '0',
-    //     password: md5('111111'),
-    //     phone: '12121212'
-    //   }
-    // const data = await this.adminService.updateById('5ce4e773dee8256ff1af299a', { nickname: '超级管理员1' })
-    // console.log(data, 'data')
-    // }
-
+    const adminExist = await this.adminService.countByCondition({ role: 0 })
+    if (!adminExist) {
+      const admin: CreateAdminDTO = {
+        nickname: '超级管理员',
+        role: '0',
+        password: md5('111111'),
+        phone: '12121212'
+      }
+      await this.adminService.create(admin)
+    }
   }
 
 }
