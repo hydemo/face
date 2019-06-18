7c1adf064ffc2dd4d0d52f483a6826bfa35f6aa6
欧阳旭靖 <>
2019-06-19 04:01:19 +0800

    修改请求频率

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
       })
     ).use(rateLimit({
       windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100 // limit each IP to 100 requests per windowMs
+      max: 10000 // limit each IP to 100 requests per windowMs
     }));
 
   const initService: InitService = app.get(InitService);
