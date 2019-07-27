9426b8c56082ca5f24041b33276e6bc75ba9cd77
欧阳旭靖 <>
2019-07-27 22:02:38 +0800

    修复monitorbug

--- src/module/resident/resident.service.ts
+++ src/module/resident/resident.service.ts
@@ -334,7 +334,7 @@ export class ResidentService {
       user: user._id,
       checkResult: 4,
       applicationTime: new Date(),
-      isMonitor,
+      isMonitor: isMonitor ? false : true,
       isPush,
       addTime: new Date(),
       type: 'family',
