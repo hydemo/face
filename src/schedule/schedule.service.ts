944c522489618d2c5fc13360ced103084fe446a6
欧阳旭靖 <oyxj1989@163.com>
2019-11-23 12:08:51 +0800

    加快p2p频率

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -296,7 +296,7 @@ export class ScheduleService {
       }))
     });
 
-    Schedule.scheduleJob('*/30 * * * * *', async () => {
+    Schedule.scheduleJob('*/15 * * * * *', async () => {
       const client = this.redis.getClient()
       const pools = await client.hkeys('p2p_pool')
       let len = pools.length
